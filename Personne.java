ac701425cb2ea90b7cb59d21397edc386952ee0b
Mamadou Siradiou Diallo <mamadousiradioudiallo4@gmail.com>
2025-10-07 10:50:21 +0200

    Ajout du getter pour le nom

--- Personne.java
+++ Personne.java
@@ -1,6 +1,20 @@
 
 public class Personne {
+    private String nom;
+    private String prenom;
+    private String adresse;
+
+    public Personne(String nom, String prenom, String adresse) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.adresse = adresse;
+    }
+
     public static void main() {
         System.out.println("hi mamadou");
+    }
+
+    public String getNom() {
+        return this.nom;
     }
 }
