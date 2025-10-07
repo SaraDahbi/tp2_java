56b2fb7762b1d2909670836c9be2d5129fd0bd26
Mamadou Siradiou Diallo <mamadousiradioudiallo4@gmail.com>
2025-10-07 10:45:09 +0200

    Création de File

---
+++ File.java
@@ -0,0 +1,11 @@
+public class File {
+    private String name;
+    private int size;
+    private String type;
+
+    public File(String name, int size, String type) {
+        this.name = name;
+        this.size = size;
+        this.type = type;
+    }
+}
